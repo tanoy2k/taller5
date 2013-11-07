1566dcec6b36cd1cf3183ff009b046a35fd0f64a
fitaliano <fitaliano@f9bc5c3d-51ac-4d06-c524-d339c327b576>
2013-11-07 21:52:09 +0000

    conectado al SQL SERVER de Ubi!

--- TEST/src/home/datos.java
+++ TEST/src/home/datos.java
@@ -24,8 +24,8 @@ public class datos {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			//String connectionUrl = "jdbc:sqlserver://localhost\\sqlexpress;"
-			String connectionUrl = "jdbc:sqlserver://200.49,156.198:1453;"
-					+ "database=BD;" + "user=java;" + "password=java";
+			String connectionUrl = "jdbc:sqlserver://200.49.156.198:1453;"
+					+ "DatabaseName=BD;" + "user=java;" + "password=java";
 			con = DriverManager.getConnection(connectionUrl);
 			System.out.println("Connected.");
 		} catch (Exception e) {
